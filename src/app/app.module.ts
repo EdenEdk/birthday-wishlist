73856ea1c5159ca0046ed9846dec23de549580b0
Eden Katabi <edenedk@gmail.com>
2021-01-11 15:11:28 +0200

    Base Style product item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app-component/app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { APP_COMPONENTS } from './components/component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [...APP_COMPONENTS],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/component.ts
@@ -0,0 +1,6 @@
+import { AppComponent } from "./app-component/app.component";
+import { HomePageComponent } from "./home-page/home-page.component";
+import { ProductsListComponent } from "./products-list/products-list.component";
+import { ProductItemComponent } from "./product-item/product-item.component";
+
+export const APP_COMPONENTS=[AppComponent,HomePageComponent, ProductsListComponent, ProductItemComponent];
